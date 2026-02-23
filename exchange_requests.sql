792abf33022c34c0ad7581ad0d514b30b5d0a5b4
D3XBd <bunditsoontorn@gmail.com>
2026-02-23 08:40:19 +0700

    Add files via upload

--- exchange_requests.sql
+++ exchange_requests.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 23, 2026 at 02:38 AM
+-- Generation Time: Feb 23, 2026 at 02:39 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.0.30
 
@@ -56,37 +56,6 @@ INSERT INTO `exchange_requests` (`id`, `user_id`, `reward_name`, `point_used`, `
 (13, 4, 'คะแนนกิจกรรม 1 ชม', 15, 'APPROVED', '2026-02-16 07:53:19'),
 (14, 1, 'คะแนนกิจกรรม 1 ชม', 15, 'PENDING', '2026-02-16 07:57:16');
 
--- --------------------------------------------------------
-
---
--- Table structure for table `users`
---
-
-CREATE TABLE `users` (
-  `id` int(11) NOT NULL,
-  `studentId` varchar(50) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `fullname` varchar(100) NOT NULL,
-  `grade` varchar(20) NOT NULL,
-  `major` varchar(100) NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
-  `raw_password` varchar(255) DEFAULT NULL,
-  `bottles` int(11) NOT NULL DEFAULT 0,
-  `points` float NOT NULL DEFAULT 0,
-  `role` varchar(20) DEFAULT 'user'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_thai_520_w2;
-
---
--- Dumping data for table `users`
---
-
-INSERT INTO `users` (`id`, `studentId`, `password`, `fullname`, `grade`, `major`, `created_at`, `raw_password`, `bottles`, `points`, `role`) VALUES
-(1, '1113', '$2y$10$xK6nAF8lizkhhg0Dl0Ur.u.yChoTlCUaYbfzEkRNP.cBoUtkYvuca', 'บัญฑิต สุนทร', 'ปวช.3', 'เทคโนโลยีสารสนเทศ', '2025-11-22 05:06:40', '123456789', 22, 875, 'user'),
-(4, '0001', '$2y$10$9F4cEsMv6dN9IM5msaHyyONAcfE0MaF1.WRZx7YMR3A8HhzJAUhoW', 'Admin', 'ปวส.2', 'เทคโนโลยีสารสนเทศ', '2026-02-15 16:55:17', '1234admin', 10009, 9954, 'ADMIN'),
-(5, '007', '$2y$10$X4.naIj0C5MicZpkJeGCQuBfvBxg4rustE041whIv8KGb4JFGWw9a', 'Patsakorn Norohem', 'ปวส.2', 'คอมพิวเตอร์ธุรกิจ', '2026-02-16 07:26:14', '123456p', 9, 4.5, 'user'),
-(6, '1234', '$2y$10$hdEFEsxblQC5V9fJS22XfuyghMFlvAUoK3Q.Tv0SQwZUQa7n4J2JC', 'test', 'ปวช.1', 'ช่างอิเล็กทรอนิกส์', '2026-02-16 07:44:56', '1234', 0, 0, 'user'),
-(7, '1111', '$2y$10$WOxbt4TgM.uylD6CYB4tnOjeM2KOi9hAoHMUK7b65GI.6uPYefYlW', 'test1', 'ปวช.1', 'คอมพิวเตอร์ธุรกิจ', '2026-02-16 07:59:57', '1111', 0, 0, 'user');
-
 --
 -- Indexes for dumped tables
 --
@@ -98,13 +67,6 @@ ALTER TABLE `exchange_requests`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `studentId` (`studentId`);
-
---
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -113,12 +75,6 @@ ALTER TABLE `users`
 --
 ALTER TABLE `exchange_requests`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
-
---
--- AUTO_INCREMENT for table `users`
---
-ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
--- users.sql
+++ users.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 23, 2026 at 02:38 AM
+-- Generation Time: Feb 23, 2026 at 02:39 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.0.30
 
@@ -20,41 +20,6 @@ SET time_zone = "+00:00";
 --
 -- Database: `bottle_exchange`
 --
-
--- --------------------------------------------------------
-
---
--- Table structure for table `exchange_requests`
---
-
-CREATE TABLE `exchange_requests` (
-  `id` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL,
-  `reward_name` varchar(100) NOT NULL,
-  `point_used` int(11) NOT NULL,
-  `status` varchar(20) DEFAULT 'PENDING',
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_thai_520_w2;
-
---
--- Dumping data for table `exchange_requests`
---
-
-INSERT INTO `exchange_requests` (`id`, `user_id`, `reward_name`, `point_used`, `status`, `created_at`) VALUES
-(1, 1, 'น้ำ 1 ขวด', 10, 'REJECTED', '2026-02-15 16:41:20'),
-(2, 1, 'น้ำ 1 ขวด', 10, 'APPROVED', '2026-02-15 16:41:27'),
-(3, 1, 'คะแนนกิจกรรม 1 ชม', 15, 'APPROVED', '2026-02-15 17:03:32'),
-(4, 1, 'น้ำ 1 ขวด', 10, 'REJECTED', '2026-02-15 17:03:34'),
-(5, 4, 'คะแนนกิจกรรม 1 ชม', 15, 'PENDING', '2026-02-15 17:07:15'),
-(6, 4, 'น้ำ 1 ขวด', 10, 'APPROVED', '2026-02-15 17:07:16'),
-(7, 4, 'คะแนนกิจกรรม 1 ชม', 15, 'REJECTED', '2026-02-15 17:14:41'),
-(8, 4, 'คะแนนกิจกรรม 1 ชม', 15, 'PENDING', '2026-02-16 01:59:41'),
-(9, 4, 'น้ำ 1 ขวด', 10, 'PENDING', '2026-02-16 01:59:44'),
-(10, 1, 'น้ำ 1 ขวด', 10, 'APPROVED', '2026-02-16 07:24:41'),
-(11, 4, 'น้ำ 1 ขวด', 10, 'APPROVED', '2026-02-16 07:25:17'),
-(12, 4, 'คะแนนกิจกรรม 1 ชม', 15, 'APPROVED', '2026-02-16 07:28:31'),
-(13, 4, 'คะแนนกิจกรรม 1 ชม', 15, 'APPROVED', '2026-02-16 07:53:19'),
-(14, 1, 'คะแนนกิจกรรม 1 ชม', 15, 'PENDING', '2026-02-16 07:57:16');
 
 -- --------------------------------------------------------
 
@@ -92,12 +57,6 @@ INSERT INTO `users` (`id`, `studentId`, `password`, `fullname`, `grade`, `major`
 --
 
 --
--- Indexes for table `exchange_requests`
---
-ALTER TABLE `exchange_requests`
-  ADD PRIMARY KEY (`id`);
-
---
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
@@ -107,12 +66,6 @@ ALTER TABLE `users`
 --
 -- AUTO_INCREMENT for dumped tables
 --
-
---
--- AUTO_INCREMENT for table `exchange_requests`
---
-ALTER TABLE `exchange_requests`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT for table `users`
